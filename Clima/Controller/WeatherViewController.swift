14e50d48b939e2ffb00ec07eead4a6a64a3b636e
U-ma-s <yuma.prodia4989@gmail.com>
2023-03-17 11:06:23 +0900

    complete

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,96 @@
+import UIKit
+import CoreLocation
+
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextFeild: UITextField!
+
+    @IBOutlet weak var backgroundImage: UIImageView!
+    var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self//位置情報を要求する前にWeatherViewControllerをデリゲートとして設定する必要がある．
+        locationManager.requestWhenInUseAuthorization()//ユーザに位置情報取得許可を求めるポップアップを表示
+        locationManager.requestLocation()//初期位置を取得
+        weatherManager.delegate = self
+        searchTextFeild.delegate = self//TextFeildとユーザーのやり取りをViewControllerに伝える.textfieldの代理(delegate)をviewcontrollerが務める
+    }
+    
+    @IBAction func locationPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+    
+
+
+}
+
+//MARK: - UITextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate{
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextFeild.endEditing(true)//searchボタンが押されたとき，テキストフィールドの編集を終了しキーボードを解除
+    }
+    /* これらのメソッドはtextFieldがトリガーとなるデリゲートメソッド -->*/
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextFeild.endEditing(true)//"Go"が押されたとき，テキストフィールドの編集を終了しキーボードを解除
+        return true
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {//ユーザーの入力を検証.
+        if textField.text != "" {//テキストフィールドに何か書いてあれば
+            return true//textFieldDidEndEditing()が実行される
+        }else{
+            textField.placeholder = "都市名を入力してください"//未入力の時，placeholderを変化させ
+            return false//テキストフィールドの編集を終了しない．
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {//searchTextFeild.endEditingがtrueになったとき呼び出される
+        if let city = searchTextFeild.text{
+            weatherManager.fetchWeather(cityName: city)
+        }
+        
+        searchTextFeild.text = ""
+    }
+    /*<---ここまで*/
+}
+
+//MARK: - WeatherManagerDelegate
+
+extension WeatherViewController:WeatherManagerDelegate {//extensionでprotcolが要求する関数を定義．WeatherManagerDelegate Prは2つの関数を要求
+    /* このメソッドはweatherManagerがトリガーとなるデリゲートメソッド -->*/
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather:WeatherModel){// _ を使うことで外部パラメータを省略可能
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString//complettion handler内からUIの更新を行うとき，メインスレッドでは実行できずバックグラウンドで実行する必要がある．
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            print(weather.conditionName)
+            self.cityLabel.text = weather.cityName
+        }
+        
+    }
+    
+    func didFeildWithError(error: Error) {
+        print(error)
+    }
+
+}
+
+//MARK: - CLLocationManagerDelegate
+extension WeatherViewController: CLLocationManagerDelegate{//CLLocationManagerのDelegateには以下の関数の実装を要求．（デフォはCLLocationManagerで既存）
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {//位置情報が返ってきたとき
+        if let location = locations.last{
+            locationManager.stopUpdatingLocation()//位置情報の取得をストップしておかないと2回目以降の呼び出しができない
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {//エラーが返ってきた時
+        print(error)
+    }
+}
---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,70 @@
+
+import Foundation
+import CoreLocation
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather:WeatherModel)
+    func didFeildWithError(error: Error)
+}
+
+struct WeatherManager {
+    let weatherURL = "{your Open Weather'API key}"
+    
+    var delegate: WeatherManagerDelegate?//WeatherViewControllerクラス側でWeatherManagerのdelegateをWeatherViewControllerに設定．
+    
+    func fetchWeather(cityName: String){
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(with: urlString)
+    }
+    
+    func fetchWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees){
+        let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitude)"
+        performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlString: String) {//withはただの外部パラメータ名//可読性の向上のため
+        //1.Create a URL
+        if let url = URL(string: urlString){//URLの作成に失敗する場合もあるのでoptional
+            
+            //2.Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            //3.Give the Session a Task
+            let task = session.dataTask(with: url) { data, respons, error in//dataTask will be finished -> closure(completion handler) will be called and do bellow
+                if error != nil {
+                    self.delegate?.didFeildWithError(error: error!)//closure内でもself不要になった？
+                    return
+                }
+                
+                if let safeData = data {
+                    if let weather = self.parseJSON(safeData){
+                        self.delegate?.didUpdateWeather(self, weather: weather)//selfは"_"で省略された外部パラメータにWeatherManager型を渡す
+                    }
+                    
+                }
+            }
+            /*↑completion handler*/
+            //4.Start a task
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ weatherData: Data) -> WeatherModel?{
+        let decoder = JSONDecoder()
+        do{
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)//decode(data type,data) //selfで型を参照
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            let weather = WeatherModel(conditionId: id, cityName: name, temprature: temp)
+            return weather
+        } catch {
+            delegate?.didFeildWithError(error: error)//delegate(WeatherViewController側で宣言されたdidFeildWithError()を呼び出すことのみ宣言，具体的な処理はdelegate側で設定)
+            return nil
+        }
+        
+    }
+    
+
+    
+}
